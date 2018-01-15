c26fb9c1ad29f48790c0f319fd7b1e69d7fc9881
Jason Simons <35230704+jaso-the-queso@users.noreply.github.com>
2018-01-15 16:26:56 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+let friends = ["Katie", "Stephen", "Vicka", "Nick", "Tyler"];
+
+for (let i = 0; i < friends.length; i++) {
+    console.log(friends[i]);
+    for (let j = 99; j > 0; j--) {
+        console.log(j + " lines of code in the file");
+    }
+}
